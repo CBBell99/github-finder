90aa1dc35b26be74d72b5bf84ee9d93eb6a39ab9
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-11 08:15:32 -0700

    create AlertContext & AlertReducer

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />}></Route>
-              <Route path='/about' element={<About />}></Route>
-              <Route path='/notfound' element={<NotFound />}></Route>
-              <Route path='/*' element={<NotFound />}></Route>
-            </Routes>
-          </main>
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/about' element={<About />}></Route>
+                <Route path='/notfound' element={<NotFound />}></Route>
+                <Route path='/*' element={<NotFound />}></Route>
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }

9cae093127a9fdce75239c6e9562a3abcf0ae480
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-12 19:08:06 -0700

    display latest git repos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false
   }
 
@@ -35,6 +36,29 @@ export const GithubProvider = ({ children }) => {
       payload: items,
     })
   };
+
+  // Get user repos
+  const getUserRepos = async (login) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10
+    })
+
+    const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    })
+  };
+
 
   // get a single user
   const getUser = async (login) => {
@@ -76,9 +100,11 @@ export const GithubProvider = ({ children }) => {
     users: state.users,
     loading: state.loading,
     user: state.user,
+    repos: state.repos,
     searchUsers,
     clearUsers,
-    getUser
+    getUser,
+    getUserRepos
   }}>
     {children}
   </GithubContext.Provider>
